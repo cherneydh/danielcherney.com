d9c3a95535a862cda8bdfa7e081da4a276f9964d
Daniel Cherney <dcherney@acm.org>
2025-02-12 20:29:24 -0800

    Updating JavaScript

--- js/home.js
+++ js/home.js
@@ -3,31 +3,30 @@ const fileData = [];
 
 async function fetchLatestArticle() {
     try {
-        const response = await fetch('https://danielcherney.com.s3.amazonaws.com/articles/');
+        const response = await fetch('https://danielcherney.com/articles/');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const text = await response.text();
         const parser = new DOMParser();
         const doc = parser.parseFromString(text, 'text/html');
-        const links = doc.querySelectorAll('a');
+        const links = Array.from(doc.querySelectorAll('a'))
+            .filter(link => link.href.endsWith('.html'));
 
         for (const link of links) {
-            if (link.href.endsWith('.html')) {
-                const articleResponse = await fetch(link.href);
-                const articleText = await articleResponse.text();
-                const articleDoc = parser.parseFromString(articleText, 'text/html');
-                const creationDate = articleDoc.querySelector('meta[name="creation-date"]');
-                const titleElement = articleDoc.querySelector('title');
+            const articleResponse = await fetch(link.href);
+            const articleText = await articleResponse.text();
+            const articleDoc = parser.parseFromString(articleText, 'text/html');
+            const creationDate = articleDoc.querySelector('meta[name="creation-date"]');
+            const titleElement = articleDoc.querySelector('title');
 
-                if (creationDate && titleElement) {
-                    fileData.push({
-                        creationDate: new Date(creationDate.content),
-                        title: titleElement.textContent,
-                        content: articleDoc.body.innerHTML,
-                        url: link.href
-                    });
-                }
+            if (creationDate && titleElement) {
+                fileData.push({
+                    creationDate: new Date(creationDate.content),
+                    title: titleElement.textContent,
+                    content: articleDoc.body.innerHTML,
+                    url: link.href
+                });
             }
         }
         fileData.sort((a, b) => b.creationDate - a.creationDate);
