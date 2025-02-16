1dec9361ad22b6d99820b2a723cce45292ab7572
Daniel Cherney <dcherney@acm.org>
2025-02-15 21:08:25 -0800

    Updating fetch

--- js/comments.js
+++ js/comments.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("comments.json")
+        fetch("https://danielcherney.com/comments", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newComment)
+        })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
